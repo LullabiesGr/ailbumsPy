d741d9968ee3e87697ddc75f782cb58f7969b193
LullabiesGr <info@lullabies.gr>
2025-05-09 12:07:23 +0300

    Update image_loader.py

--- utils/image_loader.py
+++ utils/image_loader.py
@@ -1,1 +1,13 @@
+import cv2
+import os
+
+def load_images_from_folder(folder):
+    images = {}
+    for filename in os.listdir(folder):
+        if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+            img_path = os.path.join(folder, filename)
+            img = cv2.imread(img_path)
+            if img is not None:
+                images[filename] = img
+    return images
 
